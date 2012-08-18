e4cba8688f9fd160d36f24d32a3ea8d6598feefa
Thomas Järvstrand <thomas.jarvstrand@klarna.com>
2012-08-18 15:35:38 +0200

    Create new node connections through rest-api

--- lib/edts/src/edts_dist.erl
+++ lib/edts/src/edts_dist.erl
@@ -9,7 +9,9 @@
 %%%_* Exports ==================================================================
 
 %% API
--export([connect_all/0]).
+-export([ connect/1
+        , connect_all/0
+        , make_sname/1]).
 
 %%%_* Includes =================================================================
 
@@ -21,18 +23,59 @@
 
 %%------------------------------------------------------------------------------
 %% @doc
+%% Pings Node registered with the local epmd, so that a connection is
+%% established.
+%% @end
+-spec connect(Node::atom()) -> ok.
+%%------------------------------------------------------------------------------
+connect(Node) ->
+  net_adm:ping(Node).
+
+%%------------------------------------------------------------------------------
+%% @doc
 %% Pings all nodes registered with the local epmd, so that connections are
 %% established.
 %% @end
 -spec connect_all() -> ok.
 %%------------------------------------------------------------------------------
 connect_all() ->
-    {ok, Hostname} = inet:gethostname(),
-    {ok, Nodes}    = net_adm:names(),
-    lists:foreach(fun({Name, _Port}) ->
-                          net_adm:ping(list_to_atom(Name ++ "@" ++ Hostname))
-                  end,
-                  Nodes).
+  {ok, Hostname} = inet:gethostname(),
+  {ok, Nodes}    = net_adm:names(),
+  lists:foreach(fun({Name, _Port}) ->
+                    {ok, Nodename} = make_sname(Name, Hostname),
+                    connect(Nodename)
+                end,
+                Nodes).
+
+%%------------------------------------------------------------------------------
+%% @doc
+%% Converts a string to a valid erlang sname for localhost.
+%% @end
+-spec make_sname(string()) -> atom().
+%%------------------------------------------------------------------------------
+make_sname(Name) ->
+  {ok, Hostname} = inet:gethostname(),
+  make_sname(Name, Hostname).
+
+%%------------------------------------------------------------------------------
+%% @doc
+%% Converts a string to a valid erlang sname for Hostname.
+%% @end
+-spec make_sname(Name::string(), Hostname::string()) -> atom().
+%%------------------------------------------------------------------------------
+make_sname(Name, Hostname) ->
+  try
+    {ok, list_to_atom(Name ++ "@" ++ Hostname)}
+  catch
+    _C:Reason -> {error, Reason}
+  end.
+
 
 %%%_* Internal functions =======================================================
 
+
+%%%_* Emacs ============================================================
+%%% Local Variables:
+%%% allout-layout: t
+%%% erlang-indent-level: 2
+%%% End:
--- lib/edts/src/edts_resource_nodes.erl
+++ lib/edts/src/edts_resource_nodes.erl
@@ -7,11 +7,21 @@
 -module(edts_resource_nodes).
 
 %%%_* Exports ==================================================================
--export([ content_types_provided/2
-        , init/1
-        , to_json/2]).
 
 %% API
+%% Webmachine callbacks
+-export([ allow_missing_post/2
+        , allowed_methods/2
+        , create_path/2
+        , content_types_accepted/2
+        , content_types_provided/2
+        , init/1
+        , malformed_request/2
+        , post_is_create/2]).
+
+%% Handlers
+-export([ from_json/2
+        , to_json/2]).
 
 %%%_* Includes =================================================================
 -include_lib("webmachine/include/webmachine.hrl").
@@ -20,17 +30,79 @@
 %%%_* Types ====================================================================
 %%%_* API ======================================================================
 
+
+%% Webmachine callbacks
 init(_Config) ->
-    {ok, []}.
+  {ok, []}.
+
+allow_missing_post(ReqData, Context) ->
+  {true, ReqData, Context}.
+
+allowed_methods(ReqData, Context) ->
+  {['GET', 'POST'], ReqData, Context}.
+
+content_types_accepted(ReqData, Context) ->
+  Map = [ {"application/json", from_json}],
+  {Map, ReqData, Context}.
+
 
 content_types_provided(ReqData, Context) ->
-    Map = [ {"application/json", to_json}
-          , {"text/html",        to_json}
-          , {"text/plain",       to_json}],
-    {Map, ReqData, Context}.
+  Map = [ {"application/json", to_json}
+        , {"text/html",        to_json}
+        , {"text/plain",       to_json}],
+  {Map, ReqData, Context}.
+
+create_path(ReqData, Context) ->
+  {nodename, Nodename} = lists:keyfind(nodename, 1, Context),
+  {atom_to_list(Nodename), ReqData, Context}.
+
+malformed_request(ReqData, Context0) ->
+  case wrq:method(ReqData) of
+    'GET'  -> {false, ReqData, Context0};
+    'POST' ->
+      case try_make_nodename(ReqData) of
+        {ok, Nodename} ->
+          Context = lists:keystore(nodename, 1, Context0, {nodename, Nodename}),
+          {false, ReqData, Context};
+        error ->
+          {true, ReqData, Context0}
+      end
+  end.
+
+post_is_create(ReqData, Context) ->
+  {true, ReqData, Context}.
+
+%% Handlers
 
 to_json(ReqData, Context) ->
-    {mochijson2:encode([{nodes, nodes()}]), ReqData, Context}.
+  {mochijson2:encode([{nodes, nodes()}]), ReqData, Context}.
+
+from_json(ReqData, Context) ->
+  {nodename, Nodename} = lists:keyfind(nodename, 1, Context),
+  edts_dist:connect(Nodename),
+  {true, ReqData, Context}.
 
 %%%_* Internal functions =======================================================
 
+try_make_nodename(ReqData) ->
+  try
+    NameStr = wrq:get_qs_value("node", ReqData),
+    {ok, Name} =
+      case string:chr(NameStr, $@) of
+        0 ->
+          {ok, edts_dist:make_sname(NameStr)};
+        _ ->
+          [Name0, Host] = string:tokens(NameStr, "@"),
+          {ok, edts_dist:make_sname(Name0, Host)}
+      end,
+    Name
+  catch
+    _:_ -> error
+  end.
+
+
+%%%_* Emacs ============================================================
+%%% Local Variables:
+%%% allout-layout: t
+%%% erlang-indent-level: 2
+%%% End:
