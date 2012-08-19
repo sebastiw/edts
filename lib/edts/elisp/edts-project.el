679d6b4d03b15fe8d3855063d5d9fce07e85e01c
Thomas Järvstrand <thomas.jarvstrand@klarna.com>
2012-08-19 14:21:07 +0200

    Add licensing information

--- lib/edts/elisp/edts-project.el
+++ lib/edts/elisp/edts-project.el
@@ -1,5 +1,23 @@
+;; Copyright 2012 Thomas Järvstrand <tjarvstrand@gmail.com>
+;;
+;; This file is part of EDTS.
+;;
+;; EDTS is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; EDTS is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with EDTS. If not, see <http://www.gnu.org/licenses/>.
+;;
 ;; Rudimentary project support for edts so that we can relate buffers to
 ;; projects and communicate with the correct nodes.
+
 
 (defgroup edts-project '()
   "Distel and erlang-extended-mode development tools."
--- lib/erl-complete/erl-complete-module-source.el
+++ lib/erl-complete/erl-complete-module-source.el
@@ -1,3 +1,22 @@
+;; Copyright 2012 Thomas Järvstrand <tjarvstrand@gmail.com>
+;;
+;; This file is part of EDTS.
+;;
+;; EDTS is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; EDTS is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with EDTS. If not, see <http://www.gnu.org/licenses/>.
+;;
+;; auto-complete source for erlang modules.
+
 (require 'auto-complete)
 (require 'ferl)
 
