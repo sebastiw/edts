d81fca77d09aa9734c5e3e936636052c9eeceeb9
Joao Neves <sevenjp@gmail.com>
2012-09-05 17:21:18 +0200

    Some fixes for debug mode

--- elisp/edts/edts-debug.el
+++ elisp/edts/edts-debug.el
@@ -1,14 +1,33 @@
+(defun edts-debug-toggle-breakpoint ()
+  (interactive)
+  (message "Toggle breakpoint at %s" (what-line)))
+
+(defun edts-debug-step ()
+  (interactive)
+  (message "%s" "Step"))
+
+(defun edts-debug-continue ()
+  (interactive)
+  (message "%s" "Continue"))
+
 (defun edts-enter-debug-mode ()
   (interactive)
   (pop-to-buffer "*EDTS Debugger*" nil)
   (edts-debug-mode))
+
+(defvar edts-debug-keymap
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "SPC") 'edts-debug-toggle-breakpoint)
+    (define-key map (kbd "s")   'edts-debug-step)
+    (define-key map (kbd "c")   'edts-debug-continue)
+    map))
 
 ;; EDTS debug mode
 (define-derived-mode edts-debug-mode fundamental-mode
   "EDTS debug mode"
   "Major mode for debugging interpreted Erlang code using EDTS"
   (setq buffer-read-only t)
-  (setq mode-name "EDTS-debug")
-)
+  (setq mode-name "EDTS-debug") 
+  (use-local-map edts-debug-keymap))
 
-(provide 'edts-debug-mode)
+(provide 'edts-debug)
