10ca1cd0205cec7404406b58525ef62aef5c946e
Thomas Järvstrand <tjarvstrand@gmail.com>
2012-11-25 20:51:21 +0100

    Don't track ert from emacs-master

--- install-ert.sh
+++ install-ert.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 mkdir -p "elisp/ert"
 cd "elisp/ert"
-wget -nv https://github.com/mirrors/emacs/raw/master/lisp/emacs-lisp/ert.el
-wget -nv https://github.com/mirrors/emacs/raw/master/lisp/emacs-lisp/ert-x.el
+wget -nv https://raw.github.com/ohler/ert/c619b56c5bc6a866e33787489545b87d79973205/lisp/emacs-lisp/ert.el
+wget -nv https://raw.github.com/ohler/ert/c619b56c5bc6a866e33787489545b87d79973205/lisp/emacs-lisp/ert-x.el
