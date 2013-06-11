2208090264587742e4fab471151eeb571a82c44b
Thomas Järvstrand <tjarvstrand@gmail.com>
2013-06-11 20:12:52 +0200

    Update edts-mode doc-string

--- edts-start.el
+++ edts-start.el
@@ -205,35 +205,34 @@ documentation for information on how to configure their behaviour
 further.
 
 \\{edts-mode-map}Other useful commands:
-\\[edts-buffer-node-name]                   - Print the project node-name of
-                                              current-buffer.
-\\[edts-code-compile-and-display]           - Compile current buffer and display
-                                              issues.
-\\[edts-code-eunit]                         - Run the eunit tests of current
-                                              buffer and display results.
-\\[edts-code-xref-analyze]                  - Run xref analysis on current
-                                              buffer.
-\\[edts-code-xref-analyze-related]          - Runs xref-checks for all
-                                              live buffers related to
-                                              current buffer either by
-                                              belonging to the same
-                                              project or, if current
-                                              buffer does not belong to
-                                              any project, being in the
-                                              same directory as the
-                                              current buffer's file.
-\\[edts-code-dialyze-related]               - Same as the xref-check
-                                              above, but for dialyzer.
-\\[edts-byte-compile]                       - Byte compile all EDTS elisp files.
-\\[edts-project-ensure-buffer-node-started] - Start current buffers project-node
-                                              if not already running.
-\\[edts-refactor-extract-function]          - Extract code in current region
-                                              into a separate function.
-\\[edts-register-node]                      - Register the project-node of
-                                              current buffer with the central
-                                              EDTS server.
-\\[edts-shell]                              - Start an interactive Erlang shell.
-\\[edts-start-server]                       - Start the central EDTS server."
+\\[edts-buffer-node-name]           - Display the project node-name of
+                                      current-buffer.
+\\[edts-code-compile-and-display]   - Compile current buffer and display
+                                      issues.
+\\[edts-code-xref-analyze]          - Run xref analysis on current
+                                      buffer.
+\\[edts-code-xref-analyze-related]  - Runs xref-checks for all
+                                      live buffers related to
+                                      current buffer either by
+                                      belonging to the same
+                                      project or, if current
+                                      buffer does not belong to
+                                      any project, being in the
+                                      same directory as the
+                                      current buffer's file.
+\\[edts-code-dialyze-related]       - Same as the xref-check
+                                      above, but for dialyzer.
+\\[edts-byte-compile]               - Byte compile all EDTS elisp files.
+\\[edts-project-start-node]         - Start current buffers project-node
+                                      if not already running.
+\\[edts-refactor-extract-function]  - Extract code in current region
+                                      into a separate function.
+\\[edts-init-node]                  - Register the project-node of
+                                      current buffer with the central
+                                      EDTS server.
+\\[edts-shell]                      - Start an interactive Erlang shell.
+\\[edts-start-server]               - Start the central EDTS server.
+\\[edts-man-setup]                  - Install the OTP documentation"
 
   :lighter " EDTS"
   :keymap edts-mode-map
