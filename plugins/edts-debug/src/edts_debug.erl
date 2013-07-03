de5dd61a5f55f313a51a60390af06b6a7206521c
Thomas Järvstrand <tjarvstrand@gmail.com>
2013-07-03 09:42:01 +0200

    Add edts_debug.erl

---
+++ plugins/edts-debug/src/edts_debug.erl
@@ -0,0 +1,56 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @doc
+%%% Erlang interpreter interface through a gen_server for communication
+%%% with external processes
+%%% @end
+%%% @author João Neves <sevenjp@gmail.com>
+%%% @copyright
+%%% Copyright 2012 João Neves <sevenjp@gmail.com>
+%%%
+%%% This file is part of EDTS.
+%%%
+%%% EDTS is free software: you can redistribute it and/or modify
+%%% it under the terms of the GNU Lesser General Public License as published by
+%%% the Free Software Foundation, either version 3 of the License, or
+%%% (at your option) any later version.
+%%%
+%%% EDTS is distributed in the hope that it will be useful,
+%%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%%% GNU Lesser General Public License for more details.
+%%%
+%%% You should have received a copy of the GNU Lesser General Public License
+%%% along with EDTS. If not, see <http://www.gnu.org/licenses/>.
+%%% @end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%_* Module declaration =======================================================
+-module(edts_debug).
+
+-behaviour(edts_plugin).
+
+%%%_* Exports =================================================================
+
+%% Behaviour exports
+-export([project_node_modules/0, services/0]).
+
+%%%_* Includes =================================================================
+
+%%%_* Defines ==================================================================
+
+%%%_* Types ====================================================================
+
+%%%_* API ======================================================================
+project_node_modules() -> [edts_debug_server].
+
+services() -> [edts_debug_server].
+
+%%%_* Internal functions =======================================================
+
+%%%_* Unit tests ===============================================================
+
+%%%_* Emacs ====================================================================
+%%% Local Variables:
+%%% allout-layout: t
+%%% erlang-indent-level: 2
+%%% End:
