19d790ee30b7386083b8248939fbac6a71e76a62
Joseph Dunne <joemdunne@gmail.com>
2013-08-17 23:40:26 +0100

    #12 removed redundant progn

--- elisp/edts/ferl.el
+++ elisp/edts/ferl.el
@@ -158,13 +158,12 @@ of (function-name . starting-point)."
   (save-excursion
     (let ((res nil) (oldpoint (point)))
     (goto-char (point-min))
-    (progn
-      (while (and (not res) (re-search-forward "^-export\\s-*(" oldpoint t) )
-        (erlang-skip-blank)
-        (if (eq (following-char) ?\[)
-            (progn
-              (forward-sexp)
-              (setq res (> (point) oldpoint))) ))
-      res))))
+    (while (and (not res) (re-search-forward "^-export\\s-*(" oldpoint t) )
+      (erlang-skip-blank)
+      (if (eq (following-char) ?\[)
+          (progn
+            (forward-sexp)
+            (setq res (> (point) oldpoint))) ))
+    res)))
 
 (provide 'ferl)
