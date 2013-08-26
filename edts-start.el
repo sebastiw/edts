77e85df37f0fa2fb445a598dc032764b11d5c537
Maciek Borzecki <maciek.borzecki@gmail.com>
2013-08-26 21:33:42 +0200

    Make sure that file-name is non nil

--- edts-start.el
+++ edts-start.el
@@ -121,10 +121,12 @@ Must be preceded by `erlang-font-lock-keywords-macros' to work properly.")
 consider EDTS."
   (let ((re (eproject--combine-regexps
              (cons "^\\.edts$" edts-erlang-mode-regexps)))
-        (file-name (path-util-base-name buffer-file-name)))
+        (file-name (buffer-file-name)))
     ;; dired buffer has no file
-    (if (and file-name (string-match re file-name))
-        (setq ad-return-value (reverse '(generic edts edts-temp edts-otp)))
+    (if (and file-name
+             (string-match re (path-util-base-name file-name)))
+        (setq ad-return-value (reverse '(generic
+                                         edts edts-temp edts-otp)))
       ad-do-it)))
 (ad-activate-regexp "edts-eproject-types")
 
