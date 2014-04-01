c3afae2e0100bd4b0de526282900791cabd4271f
Thomas Järvstrand <thomas.jarvstrand@klarna.com>
2014-04-01 13:19:49 +0200

    edts-mode in separate file

--- edts-start.el
+++ edts-start.el
@@ -66,20 +66,6 @@
       when dirp
       do   (add-to-list 'load-path (f-join edts-lib-directory name)))
 
-(defcustom edts-erlang-mode-regexps
-  '("^\\.erlang$"
-    "\\.app$"
-    "\\.app.src$"
-    "\\.config$"
-    "\\.erl$"
-    "\\.es$"
-    "\\.escript$"
-    "\\.eterm$"
-    "\\.script$"
-    "\\.yaws$")
-  "Additional extensions for which to auto-activate erlang-mode."
-  :group 'edts)
-
 ;; workaround to get proper variable highlighting in the shell.
 (defvar erlang-font-lock-keywords-vars
   (list
@@ -108,29 +94,6 @@ Must be preceded by `erlang-font-lock-keywords-macros' to work properly.")
                 (not (string-match ".*-test" (f-base file)))))
  do   (edts-start-load file))
 
-;; External
-(require 'auto-highlight-symbol)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; autohighlight-symbol-mode setup for EDTS
-(defconst erlang-auto-highlight-exclusions
-  (cons (quote erlang-mode)
-               (concat
-                "^\\(" erlang-operators-regexp
-                "\\|" erlang-keywords-regexp
-                "\\|\\<[[:digit:]]+\\>\\)$")))
-
-(defvar erlang-current-function-ahs-plugin
-  '((name    . "erlang current function")
-   (lighter . "CF")
-   (face    . ahs-plugin-defalt-face)
-   (start   . ferl-point-beginning-of-function)
-   (end     . ferl-point-end-of-function)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; EDTS mode
-
-
 ;; HACKWARNING!! Avert your eyes lest you spend the rest ef your days in agony
 ;;
 ;; To avoid weird eproject types like generic-git interfering with us
@@ -154,26 +117,6 @@ consider EDTS."
   :group 'convenience
   :prefix "edts-")
 
-(defvar edts-mode-map
-  (let ((map (make-sparse-keymap)))
-    (define-key map "\C-c\C-n"     'edts-code-next-issue)
-    (define-key map "\C-c\C-p"     'edts-code-previous-issue)
-    (define-key map "\C-c\C-df"    'edts-find-local-function)
-    (define-key map "\C-c\C-d\S-f" 'edts-find-global-function)
-    (define-key map "\C-c\C-dH"    'edts-find-doc)
-    (define-key map "\C-c\C-dh"    'edts-show-doc-under-point)
-    (define-key map "\C-c\C-d\C-b" 'ferl-goto-previous-function)
-    (define-key map "\C-c\C-d\C-f" 'ferl-goto-next-function)
-    (define-key map "\C-c\C-de"    'edts-ahs-edit-current-function)
-    (define-key map "\C-c\C-dE"    'edts-ahs-edit-buffer)
-    (define-key map "\C-c\C-dt"    'edts-code-eunit)
-    (define-key map "\M-."         'edts-find-source-under-point)
-    (define-key map "\M-,"         'edts-find-source-unwind)
-    map)
-  "Keymap for EDTS.")
-
-(defvar edts-mode-hook nil
-  "Hooks to run at the end of edts-mode initialization in a buffer.")
 
 (defvar edts-orig-left-margin nil
   "The original value of a buffer's left-margin width")
@@ -186,118 +129,6 @@ consider EDTS."
   "Which side to display fringe-markers on. The value must be either
 left-fringe or right-fringe."
   :group 'edts)
-
-(defun edts-setup ()
-  (edts-log-debug "Setting up edts-mode in buffer %s" (current-buffer))
-
-  ;; Start with our own stuff
-  (edts-face-remove-overlays)
-  (edts-ensure-server-started)
-  (ad-activate-regexp "edts-face.*")
-  (add-hook 'after-save-hook 'edts-code-compile-and-display t t)
-
-  ;; Auto-activate erlang mode for some additional extensions.
-  (mapcar
-   #'(lambda(re) (add-to-list 'auto-mode-alist (cons re 'erlang-mode)))
-   edts-erlang-mode-regexps)
-
-  (auto-highlight-symbol-mode t)
-  (add-to-list 'ahs-exclude erlang-auto-highlight-exclusions)
-  (make-local-variable 'ahs-case-fold-search)
-  (setq ahs-case-fold-search nil)
-
-  ;; Register the range plugin with ahs
-  (ahs-regist-range-plugin
-    erlang-current-function
-    erlang-current-function-ahs-plugin
-    "Current Erlang function")
-
-  ;; Make sure we remember our history
-  (if (boundp 'window-persistent-parameters)
-      (add-to-list 'window-persistent-parameters '(edts-find-history-ring . t))
-      (setq window-persistent-parameters '((edts-find-history-ring . t))))
-
-  ;; Ensure matching parentheses are visible above edts-faces.
-  (when (boundp 'show-paren-priority)
-    (make-local-variable 'show-paren-priority))
-  (let ((paren-prio (or
-                     (and (boundp 'show-paren-priority) show-paren-priority)
-                     0)))
-    (setq show-paren-priority
-          (max paren-prio
-               (+ 1 (apply #'max
-                           (mapcar
-                            #'cdr edts-code-issue-overlay-priorities))))))
-
-  ;; Auto-completion
-  (edts-complete-setup)
-  (run-hooks 'edts-mode-hook))
-
-(defun edts-teardown ()
-  ;; Start with our own stuff
-  (edts-face-remove-overlays)
-  (ad-deactivate-regexp "edts-.*")
-  (remove-hook 'after-save-hook 'edts-code-compile-and-display t)
-  (auto-highlight-symbol-mode -1)
-
-  ;; Remove custom value for show-paren-priority
-  (if (boundp 'show-paren-priority)
-      (kill-local-variable 'show-paren-priority)))
-
-(defvar edts-mode nil
-  "The edts mode-variable.")
-
-(define-minor-mode edts-mode
-  "An easy to set up Development-environment for Erlang. See README for
-details about EDTS.
-
-EDTS also incorporates a couple of other
-minor-modes, currently auto-highlight-mode and auto-complete-mode.
-They are configured to work together with EDTS but see their respective
-documentation for information on how to configure their behaviour
-further.
-
-\\{edts-mode-map}Other useful commands:
-\\[edts-buffer-node-name]           - Display the project node-name of
-                                      current-buffer.
-\\[edts-code-compile-and-display]   - Compile current buffer and display
-                                      issues.
-\\[edts-code-xref-analyze]          - Run xref analysis on current
-                                      buffer.
-\\[edts-code-xref-analyze-related]  - Runs xref-checks for all
-                                      live buffers related to
-                                      current buffer either by
-                                      belonging to the same
-                                      project or, if current
-                                      buffer does not belong to
-                                      any project, being in the
-                                      same directory as the
-                                      current buffer's file.
-\\[edts-dialyzer-analyze]           - Same as the xref-check
-                                      above, but for dialyzer.
-\\[edts-byte-compile]               - Byte compile all EDTS elisp files.
-\\[edts-project-start-node]         - Start current buffers project-node
-                                      if not already running.
-\\[edts-refactor-extract-function]  - Extract code in current region
-                                      into a separate function.
-\\[edts-init-node]                  - Register the project-node of
-                                      current buffer with the central
-                                      EDTS server.
-\\[edts-shell]                      - Start an interactive Erlang shell.
-\\[edts-start-server]               - Start the central EDTS server.
-\\[edts-man-setup]                  - Install the OTP documentation"
-
-  :lighter " EDTS"
-  :keymap edts-mode-map
-  :group edts
-  :require erlang-mode
-  (if edts-mode
-      (edts-setup)
-      (edts-teardown)))
-
-(defun edts-erlang-mode-hook ()
-  (when (buffer-file-name)
-    (edts-mode t)))
 
 (defun edts-byte-compile ()
   "Byte-compile all elisp packages part of EDTS."
@@ -313,6 +144,5 @@ further.
 ;; Global setup
 (edts-plugin-init-all)
 (make-directory edts-data-directory 'parents)
-(add-hook 'erlang-mode-hook 'edts-erlang-mode-hook)
 
 (provide 'edts-start)
