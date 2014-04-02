a41840804d4e9dee21aea29b1b200a1e1d9445e9
Thomas Järvstrand <thomas.jarvstrand@klarna.com>
2014-04-02 18:56:09 +0200

    Add pkg-file

---
+++ edts-pkg.el
@@ -0,0 +1,9 @@
+(define-package "edts" "master" "Erlang Development Tool Suite"
+  '((auto-complete         "20140322.321")
+    (auto-highlight-symbol "1.55")
+    (dash                  "20140327.116")
+    (eproject              "20140118.1608")
+    (erlang                "20131025.6")
+    (f                     "20140220.21")
+    (popup                 "20140207.1702")
+    (pos-tip               "20121128.615")))
