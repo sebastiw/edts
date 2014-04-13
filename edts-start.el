7c9b16c5ca77277ab3b9a3a6d812d97dbdeefd3d
Thomas Järvstrand <thomas.jarvstrand@klarna.com>
2014-04-13 22:26:11 +0200

    Add package check and erlang compilation to edts-start

--- edts-start.el
+++ edts-start.el
@@ -4,32 +4,36 @@
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; Prerequisites
-(require 'cl)
-(require 'woman)
-(require 'ert nil 'noerror)
+(defcustom edts-inhibit-package-check t
+  "If non-nil, don't check whether EDTS was installed as a package."
+  :group 'edts)
+
+(unless (or edts-inhibit-package-check
+            (and (boundp 'package-alist)
+                 (assoc 'edts package-alist)))
+  (warn (concat
+"EDTS was not installed as a package.\n"
+"\n"
+"Please see the README for details on installing EDTS from MELPA.\n"
+"\n"
+"If you know what you're doing and have all the necessary dependencies\n"
+"installed (see edts-pkg.el) you can disable this check by setting\n"
+"`edts-inhibit-package-check' to a non-nil value."))
+  (when (y-or-n-p "Do you want to disable package check now?")
+    (customize-save-variable 'edts-inhibit-package-check t)))
+
+(eval-when-compile
+  (unless load-file-name
+    (let ((default-directory edts-root-directory)
+          (buf  "*EDTS compile*"))
+      (pop-to-buffer (get-buffer-create buf))
+      (goto-char (point-max))
+      (if (= (call-process "make" nil t t "libs") 0)
+          (quit-window)
+        (error (format (concat "Failed to compile EDTS libraries. "
+                               "See %s for details.")
+                       buf))))))
 
 (require 'edts-autoloads)
-
-(defun edts-byte-compile ()
-  "Byte-compile all elisp packages part of EDTS."
-  (interactive)
-  (let* ((dirs (directory-files edts-lib-directory t "^[^.]"))
-         (files (apply #'append
-                       (mapcar #'(lambda (dir)
-                                   (directory-files dir t "\\.el$")) dirs))))
-    (byte-compile-disable-warning 'cl-functions)
-    (mapc #'byte-compile-file files)
-    t))
-
-(defun edts-start-load-tests ()
-  "Load all test-files."
-  (loop
-   for  file
-   in   (directory-files edts-code-directory nil "-test\\.el$")
-   ;; avoid symlinks created as emacs backups
-   when (not (file-symlink-p (f-join edts-code-directory file)))
-   do   (load file))
-  (edts-plugin-load-tests))
 
 (provide 'edts-start)
