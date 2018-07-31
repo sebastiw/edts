b41a0a0210f720784627860b2c370c3b43f381a6
Sebastian Weddmark Olsson <visnae@gmail.com>
2018-07-31 09:51:41 +0200

    Remove header file for remote compiled module

--- apps/edts/src/edts_event.erl
+++ apps/edts/src/edts_event.erl
@@ -26,9 +26,6 @@
 -module(edts_event).
 
 %%%_* Includes =================================================================
-
--include("otp_workarounds.hrl").
-
 %%%_* Exports ==================================================================
 
 %% API
@@ -67,6 +64,13 @@
                                      [{{class(), type()}, module()}]
                }).
 
+-ifdef(OTP_RELEASE). %% this implies 21 or higher
+-define(EXCEPTION(Class, Reason, StackToken), Class:Reason:StackToken).
+-define(GET_STACK(StackToken), StackToken).
+-else.
+-define(EXCEPTION(Class, Reason, _), Class:Reason).
+-define(GET_STACK(_), erlang:get_stacktrace()).
+-endif.
 
 %%%_* Types ====================================================================
 
