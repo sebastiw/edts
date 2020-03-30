13afc5fcd31f8afa82bd0eade7d2adde8c480a72
Sebastian Weddmark Olsson <visnae@gmail.com>
2020-03-30 19:43:20 +0200

    Add copyright and docs text to files missing it

--- edts-mode.el
+++ edts-mode.el
@@ -1,8 +1,26 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; edts-mode.el --- EDTS setup and configuration.
+
+;; Copyright 2012-2013 Thomas Järvstrand <tjarvstrand@gmail.com>
+
+;; Author: Thomas Järvstrand <thomas.jarvstrand@gmail.com>
+;; Keywords: erlang
+;; This file is not part of GNU Emacs.
+
 ;;
-;; EDTS Setup and configuration.
+;; This file is part of EDTS.
 ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; EDTS is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU Lesser General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; EDTS is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU Lesser General Public License for more details.
+;;
+;; You should have received a copy of the GNU Lesser General Public License
+;; along with EDTS. If not, see <http://www.gnu.org/licenses/>.
 
 (when (< emacs-major-version 24)
     (error "EDTS requires Emacs >= 24.0"))
